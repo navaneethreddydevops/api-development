a12b363fd93dddad74ca8bcb24d3952c1e4f7276
Navaneeth Reddy Alavala <navaneethreddydevops@gmail.com>
2024-03-16 09:08:23 -0500

    Adding packages needed

--- app/routers/post.py
+++ app/routers/post.py
@@ -18,7 +18,7 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
