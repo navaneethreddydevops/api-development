d5ef833bf730da2ef35b1ca468a5c7c433fe8f20
Navaneeth Reddy Alavala <navaneethreddydevops@gmail.com>
2024-03-17 09:04:26 -0500

    Adding packages needed

--- app/oauth2.py
+++ app/oauth2.py
@@ -4,12 +4,13 @@ from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from . import schemas, database, models
+from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
-SECRET_KEY = "hELLO"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict):
