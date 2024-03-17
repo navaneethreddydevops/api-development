ef5dcf7b21d120d04f238d1cf29b382eacd8abdf
Navaneeth Reddy Alavala <navaneethreddydevops@gmail.com>
2024-03-17 08:57:18 -0500

    Adding packages needed

--- app/config.py
+++ app/config.py
@@ -2,8 +2,17 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_password: str = "localhost"
-    database_username: str = "fastapi"
+    database_hostname: str
+    database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
+    class Config:
+        env_file = ".env"
 
 
 settings = Settings()
